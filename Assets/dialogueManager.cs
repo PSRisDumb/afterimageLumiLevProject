8c94ef37a33efb90055cac9118ae12ddb73d4851
AlyonaAES <alyonashtraizent@gmail.com>
2026-04-18 14:13:20 -0400

    dialogue stuff
    added more images, started to do sprite changes for text

--- Assets/dialogueManager.cs
+++ Assets/dialogueManager.cs
@@ -6,16 +6,18 @@ using TMPro;
 public class dialogueManager : MonoBehaviour
 {
     private string[] currentLines;
+    private int[] currentSpeaker;
     public string[] tutorialLines1;//the opening dialogue with jerry
+    public int[] tut1Speaker; //see whos talking 
     public string[] tutorialLines2;//the opening dialogue with jerry
+    public int[] tut2Speaker; //see whos talking 
     private bool tut2GO; // See if tut lines 2 has gone
     public string[] tutorialLines3;//the opening dialogue with jerry
     public string[] safeRoomLines1; // lines when the first ghost goes after you
     public string[] safeRoomLines2; // lines when the first ghost goes after you
     public string[] safeRoomLines3; // lines when the first ghost goes after you
-
     public float textSpd;
-
+    
     private int index;
     private bool mouseClicked;
 
@@ -27,14 +29,22 @@ public class dialogueManager : MonoBehaviour
     public GameObject lightSource;
     private float lineChange;
 
+    public TextMeshProUGUI explanation;
+    private bool expl1; // explains how to move and that no sanity will be lost at that pt
+    private bool expl2; // explains how to look around, how to pick up smth, and what you need to do
+    private float explT; //timer for the explinations to run
     // Start is called before the first frame update
     void Start()
     {
+        //begin the game in the dark with tut lines playing
         lightSource.SetActive(false);
         currentLines = tutorialLines1;
+        currentSpeaker = tut1Speaker;
         dialogueText.text = string.Empty;
-        StartDialogue(currentLines);
-        tut2GO = true;
+        StartDialogue(currentLines, tut1Speaker);
+
+        expl1 = true;
+        explanation.text = "";
     }
 
     // Update is called once per frame
@@ -44,7 +54,7 @@ public class dialogueManager : MonoBehaviour
         {
             if(dialogueText.text == currentLines[index])
             {
-                nextLine(currentLines);
+                nextLine(currentLines, currentSpeaker);
 
             }
             else
@@ -54,14 +64,40 @@ public class dialogueManager : MonoBehaviour
             }
         }
 
+        if (expl1)
+        {
+            explT += Time.deltaTime;
+            if (explT >= 3)
+            {
+                explanation.text = "WASD to walk";
+
+            }
+            if(explT >= 5)
+            {
+                explanation.text = "Q & E to look around";
+            }
+            if (explT >= 7)
+            {
+                explanation.text = "no sanity will be lost in this room during the tutorial";
+            }
+            if(explT >= 10) 
+             {
+                    explanation.text = "";
+                    tut2GO = true;
+                    expl1 = false;
+             }
+            
+        }
+
         if (tut2GO) {
             lineChange += Time.deltaTime;
 
-            if (lineChange >= 5f)
+            if (lineChange >= 3f)
             {
                 lightSource.SetActive(true);
                 currentLines = tutorialLines2;
-                StartDialogue(currentLines);
+                currentSpeaker = tut2Speaker;
+                StartDialogue(currentLines, tut2Speaker);
                 lineChange = 0;
                 tut2GO = false;
             }
@@ -69,22 +105,36 @@ public class dialogueManager : MonoBehaviour
         
         if (GMS.GetComponent<GMScript>().doorOpen == true)
         {
-            StartDialogue(safeRoomLines1);
+            //StartDialogue(safeRoomLines1);
         }
         
 
     }
 
-    void StartDialogue(string[] lines)
+    void StartDialogue(string[] lines, int[] speaker)
     {
         index = 0;
         dialogueBox.SetActive(true);
         dialogueText.text = string.Empty;
-        StartCoroutine(TypeLine(lines));
+        StartCoroutine(TypeLine(lines, speaker));
     }
 
-    IEnumerator TypeLine(string[] lines)
+    IEnumerator TypeLine(string[] lines, int[] speaker)
     {
+        if(speaker[index] == 0)
+        {
+            Debug.Log("jerry talking rn");
+        }
+        else if(speaker[index] == 1)
+        {
+            Debug.Log("wally talking rn");
+        }
+        else
+        {
+            Debug.Log("other wally talking rn");
+
+        }
+
         if (!mouseClicked)
         {
             foreach(char c in lines[index].ToCharArray())
@@ -100,13 +150,14 @@ public class dialogueManager : MonoBehaviour
         }
     }
 
-    void nextLine(string[] lines)
+    void nextLine(string[] lines, int[] speaker)
     {
         if  (index < lines.Length - 1)
         {
+
             index++;
             dialogueText.text = string.Empty;
-            StartCoroutine(TypeLine(lines));
+            StartCoroutine(TypeLine(lines, speaker));
         }
         else
         {
