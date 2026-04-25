d2dadfa9b9434f2ef4ba7751f3686177e730bc96
PSRisDumb <PSR_gamer0809@outlook.com>
2026-04-25 14:34:24 -0400

    Added knob puzzle and set ground work for a rework
    The knob puzzle is where you stand on a cylinder and see your heartrate and amp go up depending on a bool. Started working on changing the standing to allowing a grab where you press a or d to increase or decrease your heartrate/amp.

--- Assets/knobScript.cs
+++ Assets/knobScript.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class knobScript : MonoBehaviour
 {
+    public GameObject heartMonitor;
+
+    public bool isRateMonitor;
+    public bool canInteract;
+
     public float rate;
+    public float amp;
     public float heartRate;
+    public float height;
 
     public bool updateMonitor;
     // Start is called before the first frame update
@@ -17,13 +25,37 @@ public class knobScript : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        
+        heartMonitor.GetComponent<TextMeshPro>().SetText($"Heart rate: {Mathf.FloorToInt(heartRate)} Amp: {Mathf.FloorToInt(height)}");
+
+    }
+
+    private void OnTriggerStay(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (isRateMonitor)
+            {
+                heartRate += rate * Time.fixedDeltaTime;
+            }
+            else
+            {
+                height += amp * Time.fixedDeltaTime;
+            }
+        }
+
     }
     private void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            heartRate += rate * Time.fixedDeltaTime;
+            canInteract = true;
+        }
+    }
+    private void OnTriggerExit(Collider collisionLeave)
+    {
+        if (collisionLeave.gameObject.CompareTag("Player"))
+        {
+            canInteract = false;
         }
     }
 
